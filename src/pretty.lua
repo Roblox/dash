790281f6f5a8f313cce66ca47f0949685c133aae
Anonymous Author <roflection-bot@roblox.com>
2020-10-07 15:11:30 +0100

    Make pretty multiline and add slice

--- src/pretty.lua
+++ src/pretty.lua
@@ -8,9 +8,17 @@
 ]]
 local Dash = script.Parent
 local Types = require(Dash.Types)
-local cycles = require(Dash.cycles)
+
+local append = require(Dash.append)
 local assign = require(Dash.assign)
+local cycles = require(Dash.cycles)
 local includes = require(Dash.includes)
+local join = require(Dash.join)
+local map = require(Dash.map)
+local slice = require(Dash.slice)
+
+local concat = table.concat
+local insert = table.insert
 
 export type PrettyOptions = {
 	-- The maximum depth of ancestors of a table to display
@@ -19,6 +27,8 @@ export type PrettyOptions = {
 	omit: Types.Array<any>?,
 	-- Whether to use multiple lines (default = false)
 	multiline: boolean?,
+	-- The maximum length of a line (default = 80)
+	maxLineLength: number?,
 	-- Whether to drop the quotation marks around strings. By default, this is true for table keys
 	noQuotes: boolean?,
 	-- The indent string to use (default = "\t")
@@ -29,45 +39,83 @@ export type PrettyOptions = {
 	cycles: cycles.Cycles?,
 }
 
-local function pretty(object: any, options: PrettyOptions?): string
+local function indentLines(lines: Types.Array<string>, indent: string)
+	return map(lines, function(line)
+		return indent .. line
+	end)
+end
+
+local function prettyLines(object: any, options: PrettyOptions?): Types.Array<string>
 	options = options or {}
 	if type(object) == "table" then
 		-- A table needs to be serialized recusively
 		-- Construct the options for recursive calls for the table values
-		local valueOptions = {
-			omit = options.omit,
+		local valueOptions = assign({
+			visited = {},
+			indent = "\t"
+		}, options, {
 			-- Depth is reduced until we shouldn't recurse any more
 			depth = options.depth and options.depth - 1 or nil,
-			visited = options.visited or {},
 			cycles = options.cycles or cycles(object, {
 				visited = {},
 				refs = {},
 				nextRef = 0,
 				omit = options.omit or {}
 			})
-		}
+		})
 		if valueOptions.depth == -1 then
 			-- Indicate there is more information available beneath the maximum depth
-			return "..."
+			return {"..."}
 		end
 		if valueOptions.visited[object] then
 			-- Indicate this table has been printed already, so print a ref number instead of
 			-- printing it multiple times
-			return "&" .. valueOptions.cycles.refs[object]
+			return {"&" .. valueOptions.cycles.refs[object]}
 		end
 		
 		valueOptions.visited[object] = true
+
+		local multiline = valueOptions.multiline
+		local comma = multiline and "," or ", "
+
 		-- If the table appears multiple times in the output, mark it with a ref prefix so it can
 		-- be identified if it crops up later on
 		local ref = valueOptions.cycles.refs[object]
 		local refTag = ref and ("<" .. ref .. ">") or ""
-		local output = refTag .. "{"
-		local first = true
+		local lines = {refTag .. "{"}
+
 		-- Build the options for the recursive call for the table keys
-		local keyOptions = assign({
-			noQuotes = true
-		}, valueOptions)
+		local keyOptions = join(valueOptions, {
+			noQuotes = true,
+			multiline = false
+		})
+
+		-- Compact numeric keys into a simpler array style
+		local maxConsecutiveIndex = 0
+		local first = true
+		for index, value in ipairs(object) do
+			if valueOptions.omit and includes(valueOptions.omit, index) then
+				-- Don't include keys which are omitted
+				continue
+			end
+			if first then
+				first = false
+			else
+				lines[#lines] = lines[#lines] .. comma
+			end
+			if valueOptions.multiline then
+				local indendedValue = indentLines(prettyLines(value, valueOptions), valueOptions.indent)
+				append(lines, indendedValue)
+			else
+				lines[#lines] = lines[#lines] .. pretty(value, valueOptions)
+			end
+			maxConsecutiveIndex = index
+		end
 		for key, value in pairs(object) do
+			-- We printed a key if it's an index e.g. an integer in the range 1..n.
+			if typeof(key) == "number" and key % 1 == 0 and key >= 1 and key <= maxConsecutiveIndex then
+				continue
+			end
 			if valueOptions.omit and includes(valueOptions.omit, key) then
 				-- Don't include keys which are omitted
 				continue
@@ -75,17 +123,37 @@ local function pretty(object: any, options: PrettyOptions?): string
 			if first then
 				first = false
 			else
-				output = output .. ", "
+				lines[#lines] = lines[#lines] .. comma
 			end
-			output = output .. pretty(key, keyOptions) .. " = " .. pretty(value, valueOptions)
+			if valueOptions.multiline then
+				local keyLines = prettyLines(key, keyOptions)
+				local indentedKey = indentLines(keyLines, valueOptions.indent)
+				local valueLines = prettyLines(value, valueOptions)
+				local valueTail = slice(valueLines, 2)
+				local indendedValueTail = indentLines(valueTail, valueOptions.indent)
+				-- The last line of the key and first line of the value are concatenated together
+				indentedKey[#indentedKey] = indentedKey[#indentedKey] .. " = " .. valueLines[1]
+				append(lines, indentedKey)
+				append(lines, indendedValueTail)
+			else
+				lines[#lines] = lines[#lines] .. pretty(key, keyOptions) .. " = " .. pretty(value, valueOptions)
+			end
 		end
-		output = output .. "}"
-		return output
+		if valueOptions.multiline then
+			insert(lines, "}")
+		else
+			lines[#lines] = lines[#lines] .. "}"
+		end
+		return lines
 	elseif type(object) == "string" and not options.noQuotes then
-		return '"' .. object .. '"'
+		return {'"' .. object .. '"'}
 	else
-		return tostring(object)
+		return {tostring(object)}
 	end
+end
+
+local function pretty(object: any, options: PrettyOptions?): string
+	return concat(prettyLines(object, options), "\n")
 end
 
 return pretty
