639206c528ec958563e3a8f8b5c83efdd0b7ac71
Anonymous Author <roflection-bot@roblox.com>
2020-11-13 13:04:58 +0000

    Update collect utilities to use iterator

--- src/collect.lua
+++ src/collect.lua
@@ -8,6 +8,7 @@
 local Dash = script.Parent
 local Types = require(Dash.Types)
 local assertEqual = require(Dash.assertEqual)
+local iterator = require(Dash.iterator)
 
 -- TODO Luau: support generic function definitions
 export type CollectHandler = () -> (any, any)
@@ -16,7 +17,7 @@ local function collect(input: Types.Table, handler: CollectHandler): Types.Map<a
 	assertEqual(typeof(input), "table", [[Attempted to call Dash.collect with argument #1 of type {left:?} not {right:?}]])
 	assertEqual(typeof(handler), "function", [[Attempted to call Dash.collect with argument #2 of type {left:?} not {right:?}]])
 	local result = {}
-	for key, child in pairs(input) do
+	for key, child in iterator(input) do
 		local outputKey, outputValue = handler(key, child)
 		if outputKey ~= nil then
 			result[outputKey] = outputValue
