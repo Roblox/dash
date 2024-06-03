e9d32fc2ef55b7227f3f06e935c74d38620fe1fc
Anonymous Author <roflection-bot@roblox.com>
2024-06-03 20:12:34 +0200

    WIP: add stricter types

--- src/collect.lua
+++ src/collect.lua
@@ -1,29 +1,17 @@
 --[[
 	Collect returns a new Table derived from _input_ by iterating through its pairs and calling
-	the handler on each `(key, child)` tuple. 
+	the handler on each `(key, child)` tuple.
 
 	The handler should return a new `(newKey, value)` tuple to be inserted into the returned Table,
 	or `nil` if no value should be added.
 ]]
 local Dash = script.Parent
 local Types = require(Dash.Types)
-local assertEqual = require(Dash.assertEqual)
 local iterator = require(Dash.iterator)
 
--- TODO Luau: support generic function definitions
-export type CollectHandler = () -> (any, any)
+export type CollectHandler<Key, Value, NewKey, NewValue> = (key: Key, value: Value) -> (NewKey, NewValue) | nil
 
-local function collect(input: Types.Table, handler: CollectHandler): Types.Map<any, any>
-	assertEqual(
-		typeof(input),
-		"table",
-		[[Attempted to call Dash.collect with argument #1 of type {left:?} not {right:?}]]
-	)
-	assertEqual(
-		typeof(handler),
-		"function",
-		[[Attempted to call Dash.collect with argument #2 of type {left:?} not {right:?}]]
-	)
+local function collect<Key, Value, NewKey, NewValue>(input: Types.Map<Key, Value>, handler: CollectHandler<NewKey, NewValue>): Types.Map<NewKey, NewValue>
 	local result = {}
 	for key, child in iterator(input) do
 		local outputKey, outputValue = handler(key, child)
