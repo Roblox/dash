960bfda120f862fa43ba1e171a6891170072dea5
Anonymous Author <roflection-bot@roblox.com>
2024-06-03 19:15:43 +0200

    chore: drop type assertions for frequencies.lua

--- src/frequencies.lua
+++ src/frequencies.lua
@@ -1,16 +1,10 @@
 local Dash = script.Parent
 local Types = require(Dash.Types)
-local assertEqual = require(Dash.assertEqual)
 local reduce = require(Dash.reduce)
 
 type FrequenciesHandler = (any, any) -> any
 
 local function frequencies(input: Types.Table, handler: FrequenciesHandler?): Types.Map<any, number>
-	assertEqual(
-		typeof(input),
-		"table",
-		[[Attempted to call Dash.frequencies with argument #1 of type {left:?} not {right:?}]]
-	)
 	return reduce(input, function(acc, value, key)
 		local newKey = if handler then handler(value, key) else value
 		if acc[newKey] == nil then
