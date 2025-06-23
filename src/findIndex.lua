4a8b88587eb98f1b329faed7c2c6c7a40285b9bd
Anonymous Author <roflection-bot@roblox.com>
2025-06-23 14:41:05 +0300

    feat: reverted ipairs

--- src/findIndex.lua
+++ src/findIndex.lua
@@ -8,8 +8,7 @@
 export type FindHandler<Value> = (Value, number) -> boolean
 
 local function findIndex<Value>(input: { Value }, handler: FindHandler<Value>): number?
-	for key = 1, #input do
-		local child = input[key]
+	for key, child in ipairs(input) do
 		if handler(child, key) then
 			return key
 		end
