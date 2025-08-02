2eda95e3545406d2514aed90dc3de8a2e2be10e3
Anonymous Author <roflection-bot@roblox.com>
2025-08-01 17:12:39 -0700

    add types

--- src/memoize.lua
+++ src/memoize.lua
@@ -1,6 +1,3 @@
-local Dash = script.Parent
-local isCallable = require(Dash.isCallable)
-
 --[=[
 	Creates a function that memoizes the result of `func`. The memoized function will cache
 	results based on the arguments provided. If a resolver function is provided, it will be
@@ -12,7 +9,7 @@ local isCallable = require(Dash.isCallable)
 
 	@example
 	```lua
-	local function add(a, b)
+	local function add(a: number, b: number): number
 		return a + b
 	end
 
@@ -21,20 +18,23 @@ local isCallable = require(Dash.isCallable)
 	print(memoizedAdd(1, 2)) -- Returns from cache: 3
 
 	-- With custom resolver
-	local memoizedWithResolver = memoize(add, function(a, b)
+	local memoizedWithResolver = memoize(add, function(a: number, b: number): string
 		return string.format("%d_%d", a, b)
 	end)
 	```
 ]=]
-local function memoize(func, resolver)
-	assert(isCallable(func), "Expected a function as first argument")
-	assert(resolver == nil or isCallable(resolver), "Expected a function or nil as second argument")
 
+local Dash = script.Parent
+local Types = require(Dash.Types)
+
+export type ResolverFunction = (...any) -> string
+
+local function memoize<T...>(func: Types.AnyFunction, resolver: ResolverFunction?): Types.AnyFunction
 	local cache = {}
 
-	return function(...)
+	return function(...: T...): any
 		local args = table.pack(...)
-		local key
+		local key: string
 
 		if resolver then
 			key = resolver(...)
