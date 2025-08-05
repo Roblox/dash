9575bad555fb8475b252487b0fdb3dca45522d4f
Anonymous Author <roflection-bot@roblox.com>
2025-08-05 16:53:19 -0700

    use table.concat

--- src/memoize.lua
+++ src/memoize.lua
@@ -26,14 +26,10 @@
 
 local Dash = script.Parent
 local Types = require(Dash.Types)
-local isCallable = require(Dash.isCallable)
 
 export type ResolverFunction = (...any) -> string
 
 local function memoize(func: Types.AnyFunction, resolver: ResolverFunction?): Types.AnyFunction
-	assert(isCallable(func), "Expected a function as first argument")
-	assert(resolver == nil or isCallable(resolver), "Expected a function or nil as second argument")
-
 	local cache = {}
 
 	return function(...): any
@@ -43,11 +39,7 @@ local function memoize(func: Types.AnyFunction, resolver: ResolverFunction?): Ty
 		if resolver then
 			key = resolver(...)
 		else
-			-- Create a cache key from all arguments
-			key = ""
-			for i = 1, #args do
-				key = key .. tostring(args[i]) .. "|"
-			end
+			key = table.concat(args, "|")
 		end
 
 		if cache[key] == nil then
